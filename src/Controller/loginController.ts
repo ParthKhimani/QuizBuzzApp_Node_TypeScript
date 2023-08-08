86e7d8e2dfb5258d442db1388cc9534ef468aceb
ubuntu41 <parthkhimani48@gmail.com>
2023-08-08 19:19:07 +0530

    few changes

--- src/Controller/loginController.ts
+++ src/Controller/loginController.ts
@@ -21,13 +21,17 @@ export const adminLogin = async (req: Request, res: Response) => {
 
 export const managerLogin = async (req: Request, res: Response) => {
   const { emailId, password } = req.body;
-  const result = await Manager.findOne({ emailId: emailId });
+  const result = await Manager.findOne({ emailId: emailId }).populate(
+    "technology"
+  );
   if (!result) {
     res.status(404).json({ msg: "Manager not found !", status: "404" });
   } else {
     const passCheck = password.localeCompare(result.password);
     if (passCheck == 0) {
-      res.status(303).json({ msg: "Manager Logged In !", status: "303" });
+      res
+        .status(303)
+        .json({ msg: "Manager Logged In !", manager: result, status: "303" });
     } else {
       res.status(400).json({ msg: "Incorrect Password !", status: "400" });
     }
@@ -42,7 +46,9 @@ export const employeeLogin = async (req: Request, res: Response) => {
   } else {
     const passCheck = password.localeCompare(result.password);
     if (passCheck == 0) {
-      res.status(303).json({ msg: "Employee Logged In !", status: "303" });
+      res
+        .status(303)
+        .json({ msg: "Employee Logged In !", employee: result, status: "303" });
     } else {
       res.status(400).json({ msg: "Incorrect Password !", status: "400" });
     }
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ import { errorHandler } from "./exceptions/errorHandler";
 import "dotenv/config";
 
 const app = express();
-const PASSWORD = process.env.MONGODB_PASSWORD;
-const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,8 +14,8 @@ app.use(router);
 app.use(errorHandler);
 
 mongoose.connect(
-  `mongodb+srv://parth:${PASSWORD}@cluster0.eixcpta.mongodb.net/quiz?retryWrites=true&w=majority`
+  `mongodb+srv://parth:${process.env.MONGODB_PASSWORD}@cluster0.eixcpta.mongodb.net/quiz?retryWrites=true&w=majority`
 );
-app.listen(PORT, () => {
-  console.log(`server started at port: ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server started at port: ${process.env.PORT}`);
 });
